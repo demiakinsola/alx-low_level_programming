612fe3bfa547a8ba198de25262f98555d4bff5d5
demiakinsola <oluwademiladeakinsola@gmail.com>
2022-09-11 20:39:32 -0700

    Print all possible different combinations of three digits

--- 0x01-variables_if_else_while/101-print_comb4.c
+++ 0x01-variables_if_else_while/101-print_comb4.c
@@ -9,19 +9,26 @@ int main(void)
 {
 	int a, b, c;
 
-	a = 48;
-	b = 48;
-	c = 48;
-
-	while (a < 58)
+	for (a = 0; a < 8; a++)
 	{
-		b = 48;
-		while ( b < 58)
-		
-
+		for (b = a + 1; b < 9; b++)
+		{
+			for (c = b + 1; c < 10; c++)
+			{
+				putchar((a % 10) + '0');
+				/* % 10 means print the last digit */
+				/* + '0' tells the machine to covert the characters to numbers since putchar prints characters */ 
+				putchar((b % 10) + '0');
+				putchar((c % 10) + '0');
+				if (a == 7 && b == 8 && c == 9)
+				{
+					break;
+				}
+				putchar(',');
+				putchar(' ');
 			}
-
-			}
+		}
 	}
+	putchar('\n');
 	return (0);
 }
