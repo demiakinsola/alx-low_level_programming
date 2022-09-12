6a40f8a158630c19d715fb2c8a02e9cc78880649
demiakinsola <oluwademiladeakinsola@gmail.com>
2022-09-11 19:59:43 -0700

    Print possibble unique combinations of two digits

--- 0x01-variables_if_else_while/100-print_comb3.c
+++ 0x01-variables_if_else_while/100-print_comb3.c
@@ -1,33 +1,36 @@
 #include <stdio.h>
 /**
 *main - Entry point
-*Description: Print all different combinations of two digits
+*Description: Print all possible different combinations of two digits
 *Return: Always 0(Success)
 */
 
 int main(void)
 {
 	int a, b;
-
-	a = 48;
+	a = 48; /* 48 means 0 on the ascii table */
 	b = 48;
 
-	while (a < 58)
+	while(a < 58)
 	{
 		b = 48;
-		while (b < 58)
+		while (b < 58) /* 58 means :. It comes after 57 which means 9 */
 		{
-			if (a != b && a < b)
+			if ( a != b && a < b)
 			{
 				putchar(a);
 				putchar(b);
-				putchar(44);
-				putchar(32);
+				if (a == 56 && b == 57)
+				{
+					break;
+				}
+				putchar(44); /* 44 means comma */
+				putchar(32); /* 32 means space */
 			}
 			b++;
 		}
 		a++;
 	}
-	putchar(10);
+	putchar(10); /* 10 means print a new line */
 	return (0);
 }
---
+++ 0x01-variables_if_else_while/101-print_comb4.c
@@ -0,0 +1,27 @@
+#include <stdio.h>
+/**
+*main - Entry point
+*Description: Print all different combinations of three digits
+*Return: Always 0(Success)
+*/
+
+int main(void)
+{
+	int a, b, c;
+
+	a = 48;
+	b = 48;
+	c = 48;
+
+	while (a < 58)
+	{
+		b = 48;
+		while ( b < 58)
+		
+
+			}
+
+			}
+	}
+	return (0);
+}
