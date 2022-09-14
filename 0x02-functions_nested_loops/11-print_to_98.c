e992acc6956e4a7bd94faa6ae420fc21a10e3e68
demiakinsola <oluwademiladeakinsola@gmail.com>
2022-09-14 08:59:21 -0700

    Print from n to 98

--- 0x02-functions_nested_loops/11-print_to_98.c
+++ 0x02-functions_nested_loops/11-print_to_98.c
@@ -8,21 +8,35 @@
 
 void print_to_98(int n)
 {
-	if (n < 98)
+	while (n < 99)
 	{
-		_putchar(n + '0');
+		if (n >= 0 && n < 10)
+		{
+			_putchar((n / 10) + '0');
+		}
+		else
+		{
+			_putchar((n / 10) + '0');
+			_putchar((n % 10) + '0');
+		}
+		if (n == 98)
+		{
+			break;
+		}
 		_putchar(',');
 		_putchar(' ');
 		n++;
 	}
-
-	else if (n > 98)
+	while (n > 97)
 	{
-		_putchar(n + '0');
+		_putchar((n / 10) + '0');
+		_putchar((n % 10) + '0');
+		if (n == 98)
+		{
+			break;
+		}
 		_putchar(',');
 		_putchar(' ');
 		n--;
 	}
-	else
-	_putchar(n + '0');
 }
