d37516da27da59e0ea006884059c325fe5c2663b
demiakinsola <oluwademiladeakinsola@gmail.com>
2022-09-13 21:51:24 -0700

    Prints 10 times the lower case alphabet

---
+++ 0x02-functions_nested_loops/2-print_alphabet_x10.c
@@ -0,0 +1,26 @@
+#include "main.h"
+/**
+*Print_alphabet_x10 - Entry point
+*Description: Prints 10 times the lowercase alphabet.
+*Return: void
+*/
+
+void print_alphabet_x10(void)
+{
+	int i;
+	char c;
+
+	i = 1;
+
+	while (i < 11)
+	{
+		c = 'a'
+		while (c <= 'z')
+		{
+			_putchar(c);
+			c++;
+		}
+		_putchar('\n');
+		i++;
+	}
+}
