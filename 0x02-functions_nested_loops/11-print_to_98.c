3c9162dcca1dfbeefa9edf2a248e58b8f1f9c00e
demiakinsola <oluwademiladeakinsola@gmail.com>
2022-09-14 14:05:27 -0700

    Print all natural integers from a certain given number to 98

--- 0x02-functions_nested_loops/11-print_to_98.c
+++ 0x02-functions_nested_loops/11-print_to_98.c
@@ -1,3 +1,4 @@
+#include <stdio.h>
 #include "main.h"
 /**
 *print_to_98 - Entry point
@@ -8,35 +9,30 @@
 
 void print_to_98(int n)
 {
-	while (n < 99)
+	while (n <= 98)
 	{
-		if (n >= 0 && n < 10)
+		if (n < 98)
 		{
-			_putchar((n / 10) + '0');
+			printf("%d, ", n);
 		}
 		else
 		{
-			_putchar((n / 10) + '0');
-			_putchar((n % 10) + '0');
+			printf("%d", n);
 		}
-		if (n == 98)
-		{
-			break;
-		}
-		_putchar(',');
-		_putchar(' ');
 		n++;
 	}
-	while (n > 97)
+
+	while (n >= 98)
 	{
-		_putchar((n / 10) + '0');
-		_putchar((n % 10) + '0');
-		if (n == 98)
+		if (n > 98)
 		{
-			break;
+			printf("%d, ", n);
 		}
-		_putchar(',');
-		_putchar(' ');
+		else
+		{
+			printf("%d", n);
+		}
 		n--;
 	}
+	printf("\n");
 }
