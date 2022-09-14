8327e9b04c682f7fca4fef77d1a70a6e39259722
demiakinsola <oluwademiladeakinsola@gmail.com>
2022-09-14 04:26:13 -0700

    Add two integers and return the result

---
+++ 0x02-functions_nested_loops/10-add.c
@@ -0,0 +1,16 @@
+#include "main.h"
+/**
+*add - Entry point
+*@a: first input variable
+*@b: second input variable
+*Return: Result
+*/
+
+int add(int a, int b)
+{
+	int result;
+
+	result = a + b;
+
+	return (result);
+}
