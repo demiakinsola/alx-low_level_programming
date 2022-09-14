192761c9e6b8a8874ace543e6b09db022741ac41
demiakinsola <oluwademiladeakinsola@gmail.com>
2022-09-14 04:07:25 -0700

    Print the 9 times table starting from 0

--- 0x02-functions_nested_loops/9-times_table.c
+++ 0x02-functions_nested_loops/9-times_table.c
@@ -7,19 +7,20 @@
 
 void times_table(void)
 {
-	int a, b, c;
+	int a, b, result;
 
 	for (a = 0; a < 10; a++)
 	{
 		for (b = 0; b < 10; b++)
 		{
-			c = a * b;
-			_putchar(c + '0');
+			result = a * b;
+			_putchar(result + '0');
 			if (a == 9 && b < 10)
 			{
 				break;
 			}
 			_putchar(',');
+			_putchar(' ');
 			_putchar(' ');
 		}
 		_putchar('\n');
