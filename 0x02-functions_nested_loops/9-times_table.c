c5127f65235c45d3abdc94a25be81888723eee80
demiakinsola <oluwademiladeakinsola@gmail.com>
2022-09-14 07:23:26 -0700

    Print 9 times table from 0

--- 0x02-functions_nested_loops/9-times_table.c
+++ 0x02-functions_nested_loops/9-times_table.c
@@ -14,8 +14,25 @@ void times_table(void)
 		for (b = 0; b < 10; b++)
 		{
 			result = a * b;
-			_putchar(result + '0');
-			if (a == 9 && b < 10)
+			if (result < 10)
+			{
+				_putchar((result % 10) + '0');
+			}
+	/**
+	*The above is to get the last number for single digits
+	*since putchar prints one character at a time
+	*and the ascii table stops at integer 9.
+	*The below is to get the first and last number of two-digits one
+	*at a time. You have to convert it to the decimal value
+	of ascii table by adding 48 or '0'
+	*/
+			if (result >= 10)
+			{
+				_putchar((result / 10) + '0');
+				_putchar((result % 10) + '0');
+			}
+
+			if (b == 9)
 			{
 				break;
 			}
