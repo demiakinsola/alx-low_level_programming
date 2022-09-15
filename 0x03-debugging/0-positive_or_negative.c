831ba101f2f4597f092c35245b7a7c2c4d3d22e6
demiakinsola <oluwademiladeakinsola@gmail.com>
2022-09-15 12:54:49 -0700

    Test whether a function works by determining whether a number is positive, zero or negative

---
+++ 0x03-debugging/0-positive_or_negative.c
@@ -0,0 +1,24 @@
+#include <stdio.h>
+/**
+*positive_or_negative - Determines whether a number is positive or negative.
+*@i: Input argument
+*Return: void
+*/
+
+void positive_or_negative(int i)
+{
+	if (i > 0)
+	{
+		printf("%d is positive\n", i);
+
+	}
+
+	else if (i == 0)
+	{
+		printf("%d is zero\n", i);
+	}
+
+	else
+	{											printf("%d is negative\n", i);
+	}
+}
