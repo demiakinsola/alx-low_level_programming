c66607eb01b347b53eec071d0ee8066af5fa8d1a
demiakinsola <oluwademiladeakinsola@gmail.com>
2022-09-15 15:53:25 -0700

    This prints a square

--- 0x04-more_functions_nested_loops/8-print_square.c
+++ 0x04-more_functions_nested_loops/8-print_square.c
@@ -6,5 +6,23 @@
 */
 void print_square(int size)
 {
-	
+	int n;
+
+	n = 1;
+
+	if (size <= 0)
+	{
+		_putchar(10);
+	}
+
+	else if (size > 0)
+	{
+		while (n <= size)
+		{
+			_putchar(35);
+			n++;
+		}
+		_putchar(10);
+	}
+
 }
