95ffb8baf4ec13e256d713ecc58253e7bfc95f36
demiakinsola <oluwademiladeakinsola@gmail.com>
2022-09-15 08:08:12 -0700

    Multiply two integers

---
+++ 0x04-more_functions_nested_loops/2-mul.c
@@ -0,0 +1,19 @@
+#include <stdio.h>
+
+/**
+*mul - This multiplies two integers.
+*@a: first input argument
+*@b: second input argument
+*Return: Always 0(Success)
+*/
+
+int mul(int a, int b)
+{
+	int result;
+
+	result = a * b;
+
+	printf("%d\n", result);
+
+	return (0);
+}
--- 0x04-more_functions_nested_loops/main.h
+++ 0x04-more_functions_nested_loops/main.h
@@ -1,5 +1,7 @@
 
 
+
+int mul(int a, int b);
 int _isdigit(int c);
 int _putchar(char c);
 int _isupper(int c);
