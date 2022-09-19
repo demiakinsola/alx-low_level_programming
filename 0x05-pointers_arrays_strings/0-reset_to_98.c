d3c458c0472c1e2195f37a778054e6282d3dd5e3
demiakinsola <oluwademiladeakinsola@gmail.com>
2022-09-19 14:43:55 -0700

    Take a pointer to an int as parameter and update the value it points to to 98.

--- 0x05-pointers_arrays_strings/0-reset_to_98.c
+++ 0x05-pointers_arrays_strings/0-reset_to_98.c
@@ -10,18 +10,8 @@ void reset_to_98(int *n)
 {
 	int i;
 
-	i = 0;
 	n = &i;
 
-	if (i >= 0 || i <= 0)
-	{
-		*n = i;
-
-		*n = 98; /* This accesses the value stored in the pointers address */
+	*n = 98; /* This accesses the value stored in the pointers address */
 		/* and changes it to 98 */
-
-		i = *n;
-		_putchar(i / 10);
-		_putchar(i % 10);
-	}
 }
