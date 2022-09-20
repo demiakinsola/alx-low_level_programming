804e284e02276813e5f6823efd6212366e178b34
demiakinsola <oluwademiladeakinsola@gmail.com>
2022-09-20 11:03:39 -0700

    Return the length of a string

---
+++ 0x05-pointers_arrays_strings/2-strlen.c
@@ -0,0 +1,20 @@
+#include "main.h"
+
+/**
+* _strlen - This returns the length of a string.
+* @s: Input pointer parameter.
+* Return: length.
+*/
+
+int _strlen(char *s)
+{
+	int length;
+	int i;
+
+	for (i = 0; s[i]; i++)
+	{
+		length = i + 1;
+	}
+
+	return (length);
+}
--- 0x05-pointers_arrays_strings/main.h
+++ 0x05-pointers_arrays_strings/main.h
@@ -2,6 +2,10 @@
 #define MAIN_H
 
 
+
+void print_rev(char *s);
+void _puts(char *str);
+int _strlen(char *s);
 void swap_int(int *a, int *b);
 void reset_to_98(int *n);
 int _putchar(char c);
