90dbf0d3c1d92b29acc27d42fbecfe3a97b290d7
demiakinsola <oluwademiladeakinsola@gmail.com>
2022-09-20 20:43:33 -0700

    Print every other character of a string

--- 0x05-pointers_arrays_strings/6-puts2.c
+++ 0x05-pointers_arrays_strings/6-puts2.c
@@ -12,13 +12,17 @@ void puts2(char *str)
 	int i, j;
 
 	i = 0;
-	j = i + 1;
-/* This represents every other character */
 
+/* The value of j represents every other character */
+
+	j = i + 1;
 	for (i = 0; str[i]; i++)
 	{
 		if (i != j)
-		_putchar(str[i]);
+		{
+			_putchar(str[i]);
+			j++;
+		}
 	}
 	_putchar(10);
 }
