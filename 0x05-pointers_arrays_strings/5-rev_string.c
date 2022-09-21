c738d2a31ea2130e516b70d8aeed1a7cd0cfc967
demiakinsola <oluwademiladeakinsola@gmail.com>
2022-09-20 20:13:59 -0700

    Reverse a string and store the new values

---
+++ 0x05-pointers_arrays_strings/5-rev_string.c
@@ -0,0 +1,42 @@
+#include "main.h"
+
+/**
+* rev_string - This reverses a string.
+* This reverses a string and stores the reverse string as the new
+* content of the string.
+*
+* @s: Input pointer parameter.
+* Return: void
+*/
+
+void rev_string(char *s)
+{
+	int i, j, k;
+	char rev[100];
+	/* This is to store the reverse string initially */
+
+	for (i = 0; s[i]; i++)
+	{
+
+	}
+
+/* i will return the total number of elements including the null character. */
+	
+	k = 0;
+	for (j = i - 1 ; j >= 0; j--)
+	/* j - 1 - 1 because the index does not start from the null */
+	/* character but from the element before it */
+	
+	{
+		_putchar(s[j]);
+		rev[k] = s[j];
+		k++;
+	}
+	/* initialise k to 0 so that rev can be an array */
+
+	for (i = 0; s[i]; i++)
+	{
+		s[i] = rev[i];
+	}
+	_putchar('\n');
+}
--- 0x05-pointers_arrays_strings/main.h
+++ 0x05-pointers_arrays_strings/main.h
@@ -3,6 +3,8 @@
 
 
 
+void rev_string(char *s);
+void puts2(char *str);
 void print_rev(char *s);
 void _puts(char *str);
 int _strlen(char *s);
