9afea597c27c1388a21037a216984a601b7bc8ed
demiakinsola <oluwademiladeakinsola@gmail.com>
2022-09-22 20:11:42 -0700

    Change all owercase letters of a string to uppercase.

--- 0x06-pointers_arrays_strings/5-string_toupper.c
+++ 0x06-pointers_arrays_strings/5-string_toupper.c
@@ -3,4 +3,20 @@
 /**
 * string_toupper - This changes all lowercase letters of a string
 * to uppercase.
-* @
+* @str: Input parameter pointer.
+* Return: str.
+*/
+
+char *string_toupper(char *str)
+{
+	int i;
+
+	for (i = 0; str[i]; i++) /* Run through the string */
+	{
+		if (str[i] >= 97 && str[i] <= 122)
+		{
+			str[i] = str[i] - 32;
+		}
+	}
+	return (str);
+}
