e55af6350599099b24b2fe78abc6ca741a8acc1f
demiakinsola <oluwademiladeakinsola@gmail.com>
2022-09-22 20:47:35 -0700

    This compares two strings

--- 0x06-pointers_arrays_strings/3-strcmp.c
+++ 0x06-pointers_arrays_strings/3-strcmp.c
@@ -10,5 +10,15 @@
 
 int _strcmp(char *s1, char *s2)
 {
-	
+	int i;
+
+	for (i = 0; s1[i] == s2[i]; i++)
+	{
+		if (s1[i] == 0)
+		break;
+	}
+/* is both are equal, and s1 is 0, break and return result. */
+/* If both are not equal, return result. */
+
+	return (s1[i] - s2[i]);
 }
