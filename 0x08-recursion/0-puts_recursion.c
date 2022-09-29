08eca9ecd35edb589bc5678f8fcce52d561d6a18
demiakinsola <oluwademiladeakinsola@gmail.com>
2022-09-28 20:04:27 -0700

    Print  a string, followed by a new line.

---
+++ 0x08-recursion/0-puts_recursion.c
@@ -0,0 +1,22 @@
+#include "main.h"
+
+/**
+* _puts_recursion - This prints a string, followed by a new line.
+* @s: Input parameter pointer.
+* Return: Void.
+*/
+
+void _puts_recursion(char *s)
+{
+	int index;
+
+	index = 0;
+
+	if (*(s + index) != '\0')
+	{
+		_putchar(*(s + 0)); /* first element's value */
+		_puts_recursion(s + 1);
+	}
+
+	_putchar('\n');
+}
---
+++ 0x08-recursion/README.md
@@ -0,0 +1,2 @@
+All about Recursions(A function that calls itself).
+
