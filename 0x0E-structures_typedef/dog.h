f48b1a8bced2771cac93f4460ded3c74a7af4742
demiakinsola <oluwademiladeakinsola@gmail.com>
2022-10-10 15:16:05 -0700

    Define a new structure and its elements.

---
+++ 0x0E-structures_typedef/dog.h
@@ -0,0 +1,18 @@
+#ifndef DOG_H
+#define DOG_H
+
+/**
+* struct dog - This defines a structure and its elements.
+* @name: First inpu data type.
+* @age: Second input data type.
+* @owner: Third input data type.
+*/
+
+struct dog
+{
+	char *name;
+	float age;
+	char *owner;
+};
+
+#endif /* #define DOG_H */
