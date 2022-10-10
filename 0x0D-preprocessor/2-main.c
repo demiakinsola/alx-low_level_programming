2543495172917c3c2bab22fb10efe924c078c2da
demiakinsola <oluwademiladeakinsola@gmail.com>
2022-10-10 14:11:46 -0700

    Print the name of a file

---
+++ 0x0D-preprocessor/2-main.c
@@ -0,0 +1,14 @@
+#include <stdio.h>
+#include <string.h>
+
+/**
+* main - This prints the name of the input file.
+* Return: Always 0(Success).
+*/
+
+int main(void)
+{
+	printf("%s\n", __FILE__);
+
+	return (0);
+}
