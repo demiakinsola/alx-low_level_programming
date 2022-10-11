aaf849e0d6df3abcbfdb2d374921b61f6b81e958
demiakinsola <oluwademiladeakinsola@gmail.com>
2022-10-11 14:36:27 -0700

    A function that initializes a variable of type struct dog.

---
+++ 0x0E-structures_typedef/1-init_dog.c
@@ -0,0 +1,25 @@
+#include <stdio.h>
+#include <stdlib.h>
+#include "structure.h"
+
+/**
+ * init_dog - This initializes a variable of type struct dog.
+ * @d: Pointer to structure variable.
+ * @name: Input parameter field.
+ * @age: Input parameter field.
+ * @owner: Input parameter field.
+ * Return: Void.
+ */
+/* Declare the structure outside the function. */
+/* It is to give it a global scope. */
+/* Do this in a header file */
+
+void init_dog(struct dog *d, char *name, float age, char *owner)
+{
+/* To initialize an array, return its address and dereference pointer */
+/* You do not need to return the address of the age variable. */
+/* It was not declared as a pointer (float *age) */
+	(*d).name = name; /* d->name */
+	d->age = age;
+	(*d).owner = owner;
+}
---
+++ 0x0E-structures_typedef/structure.h
@@ -0,0 +1,11 @@
+#ifndef STRUCTURE_H
+#define STRUCTURE_H
+
+struct dog
+{
+	char *name;                        
+	float age;
+	char *owner
+};
+
+#endif /* #define STRUCTURE_H */
