17078a164b289534c459f8e2a1865fd104049883
demiakinsola <oluwademiladeakinsola@gmail.com>
2022-10-12 16:15:26 -0700

    This prints a name.

---
+++ 0x0F-function_pointers/README.md
@@ -0,0 +1,2 @@
+Function Pointers - Pointers to Functions.
+
---
+++ 0x0F-function_pointers/function_pointers.h
@@ -0,0 +1,8 @@
+#ifndef FUNCTIONS_POINTERS_H
+#define FUNCTION_POINTERS_H
+
+int _putchar(char c);
+void print_name(char *name, void (*f)(char *));
+
+
+#endif /* #define FUNCTIONS_POINTERS_H */
