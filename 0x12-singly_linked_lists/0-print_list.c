1ac2008149e36c3e6ff0a30eb77ac93833bb50b8
demiakinsola <oluwademiladeakinsola@gmail.com>
2022-10-21 13:03:37 -0700

    Fixed infinite loop error.

--- 0x12-singly_linked_lists/0-print_list.c
+++ 0x12-singly_linked_lists/0-print_list.c
@@ -14,13 +14,13 @@ size_t print_list(const list_t *h)
 	{
 		if (h->str == NULL)
 		{
-			printf("[0] (nil)");
+			printf("[0] (nil)\n");
 		}
 		else
 		{
 			printf("[%d] %s\n", h->len, h->str);
-			h = h->next; /* incrementer */
 		}
+		h = h->next; /* incrementer for linked list */
 		node++;
 	}
 	return (node);
