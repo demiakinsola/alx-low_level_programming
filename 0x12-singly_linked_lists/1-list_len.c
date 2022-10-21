68a53a4705918cbe587f742927b7d4d7a1f00040
demiakinsola <oluwademiladeakinsola@gmail.com>
2022-10-20 17:48:31 -0700

    Wrote a function that returns the number of elements in a linked structure list.

---
+++ 0x12-singly_linked_lists/1-list_len.c
@@ -0,0 +1,18 @@
+#include "lists.h"
+
+/**
+ * list_len - This  returns the number of elements in a linked list_t list.
+ * @h: Poiter to node structure.
+ * Return: Number of elements.
+ */
+
+size_t list_len(const list_t *h)
+{
+	unsigned int elements = 0; /* no. of elements */
+
+	while (h != NULL && h->str != NULL)
+	{
+		elements++;
+	}
+	return (elements);
+}
