22dad2aad517ce59a82d127e52ae0bc3175fc186
demiakinsola <oluwademiladeakinsola@gmail.com>
2022-10-24 15:12:21 -0700

    Fixed Betty coding style error.

--- 0x13-more_singly_linked_lists/2-add_nodeint.c
+++ 0x13-more_singly_linked_lists/2-add_nodeint.c
@@ -18,10 +18,10 @@ listint_t *add_nodeint(listint_t **head, const int n)
 
 	if (new_node != NULL)
 	{
-			new_node->n = n;
-			new_node->next = *head;
+		new_node->n = n;
+		new_node->next = *head;
 		/* assign first node to node after new node */
-			*head = new_node;
+		*head = new_node;
 		 /* assign new node to head pointer */
 		return (*head);
 	}
