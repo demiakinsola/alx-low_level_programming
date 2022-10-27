ba48690bdf7c168edfd71cf318d96aeefdfcc7fe
demiakinsola <oluwademiladeakinsola@gmail.com>
2022-10-26 21:05:14 -0700

    Fixed code.

--- 0x13-more_singly_linked_lists/10-delete_nodeint.c
+++ 0x13-more_singly_linked_lists/10-delete_nodeint.c
@@ -10,7 +10,7 @@
 
 int delete_nodeint_at_index(listint_t **head, unsigned int index)
 {
-	unsigned int count = 0; /* counter for index */
+	unsigned int count = 1; /* counter for index */
 	listint_t *tmp; /* holder for node to be deleted */
 
 	if (*head && index == 0)
