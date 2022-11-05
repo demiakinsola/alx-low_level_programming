c37f008e85c3dac52b275e74d00040479d08296c
demiakinsola <oluwademiladeakinsola@gmail.com>
2022-11-05 04:07:20 -0700

    Created a function that that copies the content of a file into another using system calls.

--- 0x15-file_io/3-cp.c
+++ 0x15-file_io/3-cp.c
@@ -1,21 +1,19 @@
 #include "main.h"
-
+/* function declaration */
+int read_file(char *file_from, char *buffer);
+int write_file(char *file_to, char *buffer, size_t bytes);
 /**
  * main - This copies the content of a file into another.
  * @argc: Number of argument.
  * @argv: Pointer to string
  *
- * Return: On success, 0. Otherwise, -1.
+ * Return: On success, integer. Otherwise, -1.
  */
 
 int main(int argc, char **argv)
 {
 	int ret, ret2; /* return value */
-	char *buffer;
-
-	buffer = malloc(sizeof(buffer)); /* allocate memory */
-	if (buffer == NULL)
-		return (-1);
+	char buffer[1024];
 
 	if (argc != 3)
 	{
@@ -23,6 +21,79 @@ int main(int argc, char **argv)
 		exit(97);
 	}
 	ret = read_file(argv[1], buffer);
-	ret_2 = write_file(argv[2], buffer, ret);
-	return (0);
+	ret2 = write_file(argv[2], buffer, ret);
+	dprintf(1, argv[2], ret2);
+	return (ret2);
+}
+
+/**
+ * read_file - This reads the content of a file.
+ * @file_from: file to read from.
+ * @buffer: storage to read to.
+ * Return: Number of bytes read.
+ */
+
+int read_file(char *file_from, char *buffer)
+{/* file descriptors and return values */
+	int index, fd, ret_value, ret_val2;
+
+	if (!file_from)
+		return (-1);
+	fd = open(file_from, O_RDWR);
+	if (fd == -1)
+	{
+		dprintf(2, "Error: Can't read from file %s\n", file_from);
+		exit(98);
+	}
+
+	for (index = 0; file_from[index] != '\0'; index++)
+	{
+		ret_value = read(fd, buffer, 1024);
+	}
+	if (ret_value == -1)
+		dprintf(2, "Error: Can't read from file %s\n", file_from);
+	ret_val2 = close(fd);
+	if (ret_val2 == -1)
+	{
+		dprintf(2, "Error: Can't close fd %d\n", fd);
+		exit(100);
+	}
+	return (ret_value);
+}
+
+/**
+ * write_file - This writes from buffer to a file.
+ * @file_to: File to write to.
+ * @buffer: Buffer to write from.
+ * @bytes: Number of bytes to write to a file.
+ * Return: Number of bytes written.
+ */
+
+int write_file(char *file_to, char *buffer, size_t bytes)
+{/* file descriptor and return values */
+	int fd, ret_val, ret_val3;
+
+	if (!file_to)
+		return (-1);
+	fd = open(file_to, O_RDWR | O_TRUNC);
+	if (fd == -1)
+		fd = open(file_to, O_RDWR | O_CREAT, 0664);
+			if (fd == -1)
+			{
+				dprintf(2, "Error: Can't write to %s\n", file_to);
+				exit(99);
+			}
+	ret_val = write(fd, buffer, bytes);
+	if (ret_val == -1)
+	{
+		dprintf(2, "Error: Can't write to %s\n", file_to);
+		exit(99);
+	}
+	ret_val3 = close(fd);
+	if (ret_val3 == -1)
+	{
+		dprintf(2, "Error: Can't close fd %d\n", fd);
+		exit(100);
+	}
+	return (ret_val);
 }
