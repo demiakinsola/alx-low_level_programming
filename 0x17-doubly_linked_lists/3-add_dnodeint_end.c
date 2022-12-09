715580a43b47d3057214dc06ead0fda88a271f5a
demiakinsola <oluwademiladeakinsola@gmail.com>
2022-12-09 11:32:49 -0800

    Fixed malloc issue.

---
+++ 0x17-doubly_linked_lists/3-add_dnodeint_end.c
@@ -0,0 +1,9 @@
+#include "lists.h"
+
+/**
+ * *add_dnodeint_end - This adds a new node at the end of a linked list.
+ * @head: pointer to first node.
+ * @n: data in each node.
+ *
+ * Return: address of new element or null if it fails.
+ */
