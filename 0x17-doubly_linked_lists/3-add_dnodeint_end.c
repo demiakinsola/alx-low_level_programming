95135a8002b942f556d3528a7e9fd07a4e6e3da9
demiakinsola <oluwademiladeakinsola@gmail.com>
2022-12-09 11:51:29 -0800

    Wrote  a function that adds a new node at the end of a linked list.

--- 0x17-doubly_linked_lists/3-add_dnodeint_end.c
+++ 0x17-doubly_linked_lists/3-add_dnodeint_end.c
@@ -7,3 +7,24 @@
  *
  * Return: address of new element or null if it fails.
  */
+
+dlistint_t *add_dnodeint_end(dlistint_t **head, const int n)
+{
+	dlistint_t *new_node = NULL;
+
+	new_node = malloc(sizeof(dlistint_t));
+	if (new_node == NULL) /* If malloc fails. */
+		return (NULL);
+
+	new_node->n = n;
+	if (*head != NULL)
+	{
+		*head = (*head)->next;
+	}
+	(*head)->next = new_node;
+	new_node->prev = *head;
+	new_node->next = NULL;
+	*head = new_node;
+
+	return (*head);
+}
